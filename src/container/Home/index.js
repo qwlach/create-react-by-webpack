c2eb846df54a96f3148d0680b93287f565719776
qiweilong <qiwl@maycur.com>
2021-04-23 16:55:55 +0800

    chore: 上传代码

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { Button } from 'antd';
 
 import { loadBridge } from '../../store';
+import './index.less';
 
 class Home extends PureComponent {
   
