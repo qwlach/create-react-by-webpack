1342cfaad12a9054eae658bff3b26a1100db8a88
qiweilong <qiwl@maycur.com>
2021-05-19 11:36:35 +0800

    chore: 删除代码

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react';
 import { Button } from 'antd';
 
 import { loadBridge } from '../../store';
-import './index.less';
 
 class Home extends PureComponent {
   
